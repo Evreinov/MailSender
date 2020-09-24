f2ac657b4c6a6846d4cf4f80c41cec039459c89d
Евреинов Дмитрий <d.evreinov@outlook.com>
2020-09-25 02:13:21 +0300

    Тестирование алгоритма отправки почты (консольное приложение)

--- Tests/ConsoleMailSender/Program.cs
+++ Tests/ConsoleMailSender/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Net;
+using System.Net.Mail;
+using System.IO;
 
 namespace ConsoleMailSender
 {
@@ -6,7 +9,37 @@ namespace ConsoleMailSender
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Объявить переменные содержащие e-mail'ы отправителя и получателя.
+            MailAddress from = new MailAddress("damian.ev@ya.ru", "Дмитрий Евреинов");
+            MailAddress to = new MailAddress("damian.ev@ya.ru");
+
+            // Создать письмо
+            using MailMessage message = new MailMessage(from, to)
+            {
+                Subject = "Тестовое сообщение",
+                Body = $"Текст письма: {DateTime.Now}"
+            };
+
+            // Создать клиент SMTP и отправить письмо
+            using (SmtpClient client = new SmtpClient("smtp.yandex.ru"))
+            {
+                string login, password;
+
+                // Файл с логином и паролем (включен в .gitignore)
+                using (StreamReader sr = new StreamReader(@"..\..\..\..\..\secure.txt"))
+                {
+                    login = sr.ReadLine();
+                    password = sr.ReadLine();
+                }
+
+                client.Credentials = new NetworkCredential(login, password);
+                client.EnableSsl = true;
+
+                client.Send(message);
+            }
+
+            Console.WriteLine("Письмо отправлено.");
+            Console.ReadKey();
         }
     }
 }
