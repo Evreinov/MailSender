e52447317c2ad359d757e7b791f86629e46eccf8
Евреинов Дмитрий <d.evreinov@outlook.com>
2020-09-25 03:40:26 +0300

    Создано тестовое приложение на WPF.

---
+++ Tests/WpfMailSender/App.xaml.cs
@@ -0,0 +1,6 @@
+﻿namespace WpfMailSender
+{
+    public partial class App
+    {
+    }
+}
---
+++ Tests/WpfMailSender/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Mail;
+
+namespace WpfMailSender
+{
+    public partial class MainWindow
+    {
+        public MainWindow() => InitializeComponent();
+
+        private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            MailAddress from = new MailAddress("damian.ev@ya.ru", "Дмитрий Евреинов");
+            MailAddress to = new MailAddress("damian.ev@ya.ru");
+
+            // Создать письмо
+            using MailMessage message = new MailMessage(from, to)
+            {
+                Subject = "Тестовое сообщение",
+                Body = $"Текст письма: {DateTime.Now}"
+            };
+
+            // Создать клиент SMTP и отправить письмо
+            using (SmtpClient client = new SmtpClient("smtp.yandex.ru"))
+            {
+                string login = LoginEdit.Text; 
+                var password = PasswordEdit.SecurePassword;
+
+                client.Credentials = new NetworkCredential(login, password);
+                client.EnableSsl = true;
+
+                client.Send(message);
+            }
+        }
+    }
+}
