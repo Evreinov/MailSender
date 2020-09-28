932c3f5b933317b9604868e500b6e88706c451d0
Евреинов Дмитрий <d.evreinov@outlook.com>
2020-09-29 01:04:08 +0300

    Созданы тестовые данные.

---
+++ MailSender/Data/TestData.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using MailSender.Models;
+
+namespace MailSender.Data
+{
+    static class TestData
+    {
+        public static List<Sender> Senders { get; } = Enumerable.Range(1, 20)
+            .Select(i => new Sender
+            {
+                Id = i,
+                Name = $"Отправитель {i}",
+                Address = $"sender_{i}@server.ru",
+            })
+            .ToList();
+
+        public static List<Recipient> Recipients { get; } = Enumerable.Range(1, 20)
+            .Select(i => new Recipient
+            {
+                Id = i,
+                Name = $"Получатель {i}",
+                Address = $"recipient_{i}@server.ru",
+            })
+            .ToList();
+
+        public static List<Server> Servers { get; } = Enumerable.Range(1, 5)
+            .Select(i => new Server
+            {
+                Id = i,
+                Name = $"Имя {i}",
+                Address = $"smtp.server{i}",
+                Login = $"Пользователь {i}",
+                Password = $"Пароль{i}",
+                UseSSL = i % 2 == 0
+            })
+            .ToList();
+
+        public static List<Message> messages { get; } = Enumerable.Range(1, 20)
+            .Select(i => new Message
+            {
+                Id = i,
+                Title = $"Заголовок {i}",
+                Body = $"Текст сообщения {i}."
+            })
+            .ToList();
+    }
+}
--- MailSender/Models/Server.cs
+++ MailSender/Models/Server.cs
@@ -7,9 +7,20 @@ namespace MailSender.Models
     class Server
     {
         public int Id { get; set; }
-        public String Name { get; set; }
-        public String Address { get; set; }
-        public int Port { get; set; } = 25;
+        public string Name { get; set; }
+        public string Address { get; set; }
+
+        private int _Port = 25;
+        public int Port
+        {
+            get => _Port;
+            set
+            {
+                if (value < 0 || value > 65535)
+                    throw new ArgumentOutOfRangeException(nameof(value), value, "Номер порта должен быть в диапазоне от 0 до 65535");
+                _Port = value;
+            }
+        }
         public bool UseSSL { get; set; }
         public string Login { get; set; }
         public string Password { get; set; }
