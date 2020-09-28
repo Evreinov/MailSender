13296fa2b9287dc51fc2a58dac50f31f1485884e
Евреинов Дмитрий <d.evreinov@outlook.com>
2020-09-29 00:25:44 +0300

    Создание модели с заполнением свойств классов.

---
+++ MailSender/Models/Server.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MailSender.Models
+{
+    class Server
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String Address { get; set; }
+        public int Port { get; set; } = 25;
+        public bool UseSSL { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Description { get; set; }
+    }
+}
