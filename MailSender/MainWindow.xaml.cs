7981655eb2ec7d7c372e47aa32853a73b701ceb4
Евреинов Дмитрий <d.evreinov@outlook.com>
2020-09-29 14:39:45 +0300

    Реализована логика управления списками серверов.

--- MailSender/MainWindow.xaml.cs
+++ MailSender/MainWindow.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System.Diagnostics;
 using System.Net.Mail;
 using System.Windows;
+using System.Linq;
 using MailSender.lib;
 using MailSender.Models;
+using MailSender.Data;
 
 namespace MailSender
 {
@@ -10,7 +12,81 @@ namespace MailSender
     {
         public MainWindow() => InitializeComponent();
 
-        private void OnSendButtonClick(object Sender, System.Windows.RoutedEventArgs E)
+        private void OnAddServerButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (!ServerEditDialog.Create(
+                out var name, 
+                out var address, 
+                out var port, 
+                out var ssl, 
+                out var description,
+                out var login,
+                out var password)) 
+                return;
+            var server = new Server
+            {
+                Id = TestData.Servers.DefaultIfEmpty().Max(s => s.Id) + 1,
+                Name = name,
+                Address = address,
+                Port = port,
+                UseSSL = ssl,
+                Description = description,
+                Login = login,
+                Password = password
+            };
+
+            TestData.Servers.Add(server);
+            ServersList.ItemsSource = null;
+            ServersList.ItemsSource = TestData.Servers;
+            ServersList.SelectedItem = server;
+        }
+
+        private void OnEditServerButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (!(ServersList.SelectedItem is Server server)) return;
+
+            var name = server.Name;
+            var address = server.Address;
+            var port = server.Port;
+            var ssl = server.UseSSL;
+            var description = server.Description;
+            var login = server.Login;
+            var password = server.Password;
+
+            if (!ServerEditDialog.ShowDialog(
+                "Редактирование сервера",
+                ref name,
+                ref address,
+                ref port,
+                ref ssl,
+                ref description,
+                ref login,
+                ref password)) 
+                return;
+
+            server.Name = name;
+            server.Address = address;
+            server.Port = port;
+            server.UseSSL = ssl;
+            server.Description = description;
+            server.Login = login;
+            server.Password = password;
+
+            ServersList.ItemsSource = null;
+            ServersList.ItemsSource = TestData.Servers;
+        }
+
+        private void OnDeleteServerButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (!(ServersList.SelectedItem is Server server)) return; 
+
+            TestData.Servers.Remove(server);
+            ServersList.ItemsSource = null;
+            ServersList.ItemsSource = TestData.Servers;
+            ServersList.SelectedItem = TestData.Servers.FirstOrDefault();
+        }
+
+        private void OnSendButtonClick(object _sender, System.Windows.RoutedEventArgs e)
         {
             if (!(SendersList.SelectedItem is Sender sender)) return;
             if (!(RecipientsList.SelectedItem is Recipient recipient)) return;
@@ -37,10 +113,10 @@ namespace MailSender
                     MessageBoxButton.OK,
                     MessageBoxImage.Information);
             }
-            catch (SmtpException e)
+            catch (SmtpException ex)
             {
                 MessageBox.Show(
-                    "Ошибка при отправке почты " + e.Message, 
+                    "Ошибка при отправке почты " + ex.Message, 
                     "Отправка почты",
                     MessageBoxButton.OK, 
                     MessageBoxImage.Error);
