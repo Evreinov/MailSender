ef78d24191100b3cbb09ededee43536a03d702d5
Евреинов Дмитрий <d.evreinov@outlook.com>
2020-10-02 11:09:16 +0300

    Тестовая реализация модели представления.

---
+++ Tests/WpfTest/ViewModels/Base/ViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace WpfTest.ViewModels.Base
+{
+    abstract class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        //delegate void PropertyChangedEventHandler(object sender, PropertyChangedEventArgs e);
+        protected virtual void OnPropertyChange(string PropertyName )
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+    }
+}
---
+++ Tests/WpfTest/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using WpfTest.ViewModels.Base;
+
+namespace WpfTest.ViewModels
+{
+    class MainWindowViewModel : ViewModel
+    {
+        private string _Title = "Тестовое окно";
+        public string Title
+        {
+            get => _Title;
+            set
+            {
+                if (_Title == value) return;
+                _Title = value;
+                OnPropertyChange("Title");
+            }
+        }
+    }
+}
