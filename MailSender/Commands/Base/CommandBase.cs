375f9ab8549cf3082a6ba01647a12bf0b9dfc509
Евреинов Дмитрий <d.evreinov@outlook.com>
2020-10-05 00:09:24 +0300

    Созданы базовые классы для MVVM.

---
+++ MailSender/Commands/Base/CommandBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace MailSender.Commands.Base
+{
+    abstract class CommandBase : ICommand
+    {
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        bool ICommand.CanExecute(object parameter) => CanExecute(parameter);
+        void ICommand.Execute(object parameter) => Execute(parameter);
+        protected virtual bool CanExecute(object parameter) => true;
+        protected abstract void Execute(object parameter);
+    }
+}
