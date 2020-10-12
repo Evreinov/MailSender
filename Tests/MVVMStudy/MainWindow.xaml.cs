5c69f4a49e36f9df6a82451a74b00b17f55d42ef
Евреинов Дмитрий <d.evreinov@outlook.com>
2020-10-13 02:56:21 +0300

    Создана модель-представления и реализован паттерн MVVM.

--- Tests/MVVMStudy/MainWindow.xaml.cs
+++ Tests/MVVMStudy/MainWindow.xaml.cs
@@ -1,48 +1,11 @@
-﻿using MVVMStudy.Models;
-using MVVMStudy.Commands;
-using System.Windows.Input;
-using System.Collections.Generic;
-using System.Linq;
-using System.Windows;
-using System.Collections.ObjectModel;
-
+﻿using MVVMStudy.ViewModels;
 
 namespace MVVMStudy
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
-        private ICommand _changeColorCommand = null;
-        public ICommand ChangeColorCommand => _changeColorCommand ?? (_changeColorCommand = new ChangeColorCommand());
+        public MainWindowsViewModel ViewModel { get; set; } = new MainWindowsViewModel();
+        public MainWindow() => InitializeComponent();
 
-        private ICommand _addCarCommand = null;
-        public ICommand AddCarCommand => _addCarCommand ?? (_addCarCommand = new AddCarCommand());
-
-        private RelayCommand<Inventory> _deleteCarCommand = null;
-        public RelayCommand<Inventory> DeleteCarCommand => _deleteCarCommand ?? (_deleteCarCommand = new RelayCommand<Inventory>(DeleteCar, CanDeleteCar));
-
-        private bool CanDeleteCar(Inventory сar) => сar != null;
-
-        private void DeleteCar(Inventory car)
-        {
-            _cars.Remove(car);
-        }
-
-        readonly IList<Inventory> _cars = new ObservableCollection<Inventory>();
-        public MainWindow()
-        {
-            InitializeComponent();
-            _cars.Add(new Inventory { CarId = 1, Color = "Blue", Make = "Chevy", PetName = "Kit", IsChanged=false });
-            _cars.Add(new Inventory { CarId = 2, Color = "Red", Make = "Ford", PetName = "Red Reider", IsChanged=false });
-            cboCars.ItemsSource = _cars;
-        }
-
-        private void BtnAddCar_OnClick(object sender, RoutedEventArgs e)
-        {
-            var maxCount = _cars?.DefaultIfEmpty().Max(x => x.CarId) ?? 0;
-            _cars?.Add(new Inventory { CarId = ++maxCount, Color = "Yeloow", Make = "VM", PetName = "Birdie", IsChanged = false });
-        }
     }
 }
---
+++ Tests/MVVMStudy/ViewModels/MainWindowsViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using MVVMStudy.Commands;
+using MVVMStudy.Models;
+
+namespace MVVMStudy.ViewModels
+{
+    public class MainWindowsViewModel
+    {
+        public IList<Inventory> Cars { get; } = new ObservableCollection<Inventory>();
+        public MainWindowsViewModel()
+        {
+            Cars.Add(new Inventory { CarId = 1, Color = "Blue", Make = "Chevy", PetName = "Kit", IsChanged = false });
+            Cars.Add(new Inventory { CarId = 2, Color = "Red", Make = "Ford", PetName = "Red Reider", IsChanged = false });
+        }
+
+        private ICommand _changeColorCommand = null;
+        public ICommand ChangeColorCommand => _changeColorCommand ?? (_changeColorCommand = new ChangeColorCommand());
+
+        private ICommand _addCarCommand = null;
+        public ICommand AddCarCommand => _addCarCommand ?? (_addCarCommand = new AddCarCommand());
+
+        private RelayCommand<Inventory> _deleteCarCommand = null;
+        public RelayCommand<Inventory> DeleteCarCommand => _deleteCarCommand ?? (_deleteCarCommand = new RelayCommand<Inventory>(DeleteCar, CanDeleteCar));
+
+        private bool CanDeleteCar(Inventory сar) => сar != null;
+
+        private void DeleteCar(Inventory car)
+        {
+            Cars.Remove(car);
+        }
+    }
+}
